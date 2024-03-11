7a42b12ffe05f25a81abde5fb85564d4e0e48d4f
unknown <seanghuon93@gmail.com>
2024-03-11 15:38:16 +0700

    food-panda

---
+++ php/index.php
@@ -0,0 +1,44 @@
+<?php include('../db/db.php') ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- @Link Css -->
+    <link rel="stylesheet" href="../css/style.css">
+    <!-- @Link Boostrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- @Link Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+    <!-- @Link Sweet Alert -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js" integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!-- @Link Jqurey -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="" method="post">
+        <input type="text" name="testsql" id="">
+        <input type="submit" name="test1" value="click me">
+        <?php
+
+        if (isset($_POST['test1'])) {
+            $input = $_POST['testsql'];
+            $sql = mysqli_query($connection, "INSERT INTO `users`(`name`) VALUE ('$input') ");
+            if ($sql) {
+                echo 'success';
+            } else {
+                echo 'error sql';
+            }
+        }
+
+        ?>
+    </form>
+
+</body>
+
+
+</html>
